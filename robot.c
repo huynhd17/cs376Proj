14e774b04a61b8a0eb80f1ee2fa678ded5128cd9
David Huynh <huynhd17@up.edu>
2014-10-09 01:53:13 +0000

    Added David Huynh to robot.c

--- robot.c
+++ robot.c
@@ -25,14 +25,14 @@ void robPrintMessage(int num)
 {
   int i = 0;
 
-  printf("I ");
+  printf("David Huynh ");
 
   for(i; i < num; i++)
     {
       printf("really ");
     }
 
-  printf("love robots!\n");
+  printf("loves robots!\n");
 
   return;
 }
